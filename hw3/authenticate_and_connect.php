66096468ee1930e493dcef6c37258759015a6640
KshitijPingle <111930104+KshitijPingle@users.noreply.github.com>
2026-03-14 20:43:58 -0700

    Made a function to send authentication headers

--- hw3/authenticate_and_connect.php
+++ hw3/authenticate_and_connect.php
@@ -3,6 +3,17 @@
 
 require_once('/protected/Adaptation.php');
 
+function sendAuthenticationHeader() {
+    // Sends an authentication header to the browser
+    //  This tells the browser to ask user for username and password
+    //  Allows us to be able to use '$_SERVER['PHP_AUTH_USER]
+    
+    header('WWW-Authenticate: Basic realm="Team Portal"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Please enter your username and password';
+    exit;
+}
+
 function authenticate_and_connect() {
     static $my_db_connection = null;
 
@@ -11,7 +22,7 @@ function authenticate_and_connect() {
         return;
     }
 
-    
+
 }
 
 ?>
